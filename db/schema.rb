7ba6c13dcd80e62f9c854451dd4d1ce79b53821c
Ciprian Florescu <ciprian.florescu@softwire.com>
2015-11-06 15:43:46 +0200

    Make email nullabe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106103717) do
+ActiveRecord::Schema.define(version: 20151106134055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20151106103717) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "name",                   default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "image",                  default: "", null: false
